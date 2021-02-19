e375e2b3ccb8db883cf23d0400a12483e3ea6eb6
soyoung <so_young_jung@naver.com>
2021-02-19 17:56:07 +0900

    TEST: Spring Security적용으로 깨진 테스트 수정

---
+++ src/main/java/com/jojodu/book/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.jojodu.book.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
